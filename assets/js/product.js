56477a72a347764d47dfdca091dd596fecba1f02
Erick Melo <erick_melo10@outlook.com>
2025-02-15 11:31:09 -0300

    Finalizado a página de checkout

---
+++ assets/js/product.js
@@ -0,0 +1,12 @@
+// area de descrição 
+
+let descButton = document.querySelector('.desc-header .btn-icon');
+let descBody = document.querySelector('.desc-body');
+
+descButton.addEventListener('click', ()=>{
+  if (descBody.style.display === 'none') {
+    descBody.style.display = 'block';
+  } else {
+    descBody.style.display = 'none';
+  }
+});
